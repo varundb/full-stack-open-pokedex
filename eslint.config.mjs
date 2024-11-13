513d28ae0c610119a6a11aefc986bac294705a3f
varundb <varundb@gmail.com>
2024-11-13 17:13:53 -0500

    Update eslint.config.mjs with more config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,30 @@
 import globals from 'globals'
 
+import stylisticJs from '@stylistic/eslint-plugin-js'
+
 export default [
   {
-    files: ["**/*.js"],
-    languageOptions: {
-      sourceType: "commonjs",
-      globals: {
-        ...globals.node,
-      },
-      ecmaVersion: "latest",
+    // ...
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
     },
   },
 ]
