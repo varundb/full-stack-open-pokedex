8472739109106ac522d8b54d205b039eaa53d5ff
varundb <varundb@gmail.com>
2024-11-13 17:18:00 -0500

    Update eslint.config.mjs additional config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,16 @@ import globals from 'globals'
 import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
+  js.configs.recommended,
   {
-    // ...
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+    },
     plugins: {
       '@stylistic/js': stylisticJs
     },
